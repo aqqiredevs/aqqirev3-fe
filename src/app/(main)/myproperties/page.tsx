a08270b7a3be4ecf581161d6bef8f2b03a5cc909
aqqiredevs <aqqiredevs@eflexervices.com>
2025-12-11 02:00:53 +0800

    updated repo

--- src/app/(main)/myproperties/page.tsx
+++ src/app/(main)/myproperties/page.tsx
@@ -1,19 +1,32 @@
-import React from 'react'
-import Form from "@/components/properties/property/form"
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Form from "@/components/properties/property/form";
+import { apiInstance } from "@/utils/axiosInstance";
+import { checkErrors } from "@/utils/checkError";
 
 const MyPropertiesPage = () => {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    const displayMyProperties = async () => {
+      try {
+        const res = await apiInstance.get(`/properties/my-properties`);
+        console.log(res.data);
+        setProperties(res.data);
+      } catch (err) {
+        checkErrors(err);
+      }
+    };
+
+    displayMyProperties();
+  }, []);
+
   return (
     <div>
-        <div>
-       
-
-
-        </div>
-
-
-
+      <div>My Properties</div>
     </div>
-  )
-}
+  );
+};
 
-export default MyPropertiesPage
+export default MyPropertiesPage;
--- src/components/properties/myProperties/UploadProperty.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const UploadProperty = () => {
-  return (
-    <div>UploadProperty</div>
-  )
-}
-
-export default UploadProperty
